0edd94b7b64195871599f5646c4c215e0cd72bdb
Rafael M Mudafort <rafmudaf@gmail.com>
2019-02-07 15:27:43 -0700

    Update simulation code interfaces

--- wind_farm_controls_tools/floris_utilities.py
+++ wind_farm_controls_tools/floris_utilities.py
@@ -1,3 +1,55 @@
+#
+# Copyright 2019 NREL
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+# this file except in compliance with the License. You may obtain a copy of the
+# License at http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+
+import pandas as pd
+import numpy as np
+from .generic_simulation import GenericInterface
+from floris.floris import Floris
+from .flow_field import FlowField
+
+# Performance Analysis Uncertainty Library And FLow EstiMation plottING: PAUL A FLEMING
+
 
 class FlorisInterface(GenericInterface):
-    pass
+    """
+    The interface from FLORIS to the wfc tools
+    """
+
+    def __init__(self, input_file):
+        self.input_file = input_file
+        self.floris = Floris(input_file=input_file)
+        
+        super().__init__(self)
+
+    def run_floris(self):
+        self.floris.farm.flow_field.calculate_wake()
+
+    def get_flow_field(self, resolution=None):
+        """
+        df: a pandas table with the columns, x,y,z,u,v,w of all relevant flow info
+            origin: the origin of the flow field, for reconstructing turbine coords
+        Paul Fleming, 2018
+        """
+
+        flow_field = self.floris.farm.flow_field
+        if resolution is not None:
+            # TODO: flow_field.redo_resolution()
+            pass
+        x = flow_field.x.flatten()
+        y = flow_field.y.flatten()
+        z = flow_field.z.flatten()
+        u = flow_field.u_field.flatten()
+        if hasattr(flow_field, 'v'):
+            v = flow_field.v.flatten()
+            w = flow_field.w.flatten()
+        return FlowField(x, y, z, u, v, w)
--- wind_farm_controls_tools/sowfa_utilities.py
+++ wind_farm_controls_tools/sowfa_utilities.py
@@ -16,3 +16,54 @@ class SowfaData(GenericInterface):
         #
 
         super.__init__(wind_speed)
+
+
+        def read_flow_frame_SOWFA(filename):
+    """Read flow array output from SOWFA
+
+
+    input: filename: name of flow array to open
+
+    output:
+		df: a pandas table with the columns, x,y,z,u,v,w of all relavent flow info
+        origin: the origin of the flow field, for reconstructing turbine coords
+
+    Paul Fleming, 2018 """
+
+    # Read the dimension info from the file
+    with open(filename, 'r') as f:
+        for i in range(10):
+            read_data = f.readline()
+            if 'SPACING' in read_data:
+                spacing = tuple([float(d)
+                                 for d in read_data.rstrip().split(' ')[1:]])
+            if 'DIMENSIONS' in read_data:
+                dimensions = tuple([float(d)
+                                    for d in read_data.rstrip().split(' ')[1:]])
+            if 'ORIGIN' in read_data:
+                origin = tuple([float(d)
+                                for d in read_data.rstrip().split(' ')[1:]])
+
+    # Set up x, y, z as lists
+    if dimensions[0] > 1.0:
+        xRange = np.arange(0, dimensions[0]*spacing[0], spacing[0])
+    else:
+        xRange = np.array([0.0])
+    if dimensions[1] > 1.0:
+        yRange = np.arange(0, dimensions[1]*spacing[1], spacing[1])
+    else:
+        yRange = np.array([0.0])
+    if dimensions[2] > 1.0:
+        zRange = np.arange(0, dimensions[2]*spacing[2], spacing[2])
+    else:
+        zRange = np.array([0.0])
+
+    pts = np.array([(x, y, z) for z in zRange for y in yRange for x in xRange])
+
+    df = pd.read_csv(filename, skiprows=10, sep='\t',
+                     header=None, names=['u', 'v', 'w'])
+    df['x'] = pts[:, 0]
+    df['y'] = pts[:, 1]
+    df['z'] = pts[:, 2]
+
+    return df, spacing, dimensions, origin
